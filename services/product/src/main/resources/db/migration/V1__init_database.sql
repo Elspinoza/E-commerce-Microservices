c316f6a674b45a388297b70e2653d8c8c253d394
Innocent ACOLATSE <inolatse@gmail.com>
2024-07-08 10:35:09 +0000

    Add 08/07/2024/09:42 ----------------------------------------- Update Product Table with Flyway and postgresql query

--- services/product/src/main/resources/db/migration/V1__init_database.sql
+++ services/product/src/main/resources/db/migration/V1__init_database.sql
@@ -3,7 +3,7 @@ create table if not exists category
     id integer not null primary key,
     name varchar(255),
     description varchar(255)
-);
+    );
 
 create table if not exists product
 (
@@ -13,8 +13,8 @@ create table if not exists product
     available_quantity double precision not null,
     price numeric(38, 2),
     category_id integer
-        constraint fk1mzegsvzgfzezr references category
-);
+    constraint fk1mzegsbhuguvzgfzezr references category
+    );
 
 create sequence if not exists category_seq increment by 50;
 create sequence if not exists product_seq increment by 50;
