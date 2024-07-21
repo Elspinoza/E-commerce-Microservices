848653fad192ab3277c1128b595198629f221461
Innocent ACOLATSE <inolatse@gmail.com>
2024-07-21 14:22:45 +0000

    Add 21/07/2024 --------------------------------------------------
    Payment package updated

--- services/payment/src/main/java/com/kamis/ecommerce/payment/PaymentMapper.java
+++ services/payment/src/main/java/com/kamis/ecommerce/payment/PaymentMapper.java
@@ -3,12 +3,16 @@ package com.kamis.ecommerce.payment;
 import org.springframework.stereotype.Service;
 @Service
 public class PaymentMapper {
+
     public Payment toPayment(PaymentRequest request) {
+
+        if (request == null) return null;
+
         return Payment.builder()
                 .id(request.id())
-                .orderId(request.orderId())
                 .paymentMethod(request.paymentMethod())
                 .amount(request.amount())
+                .orderId(request.orderId())
                 .build();
     }
 }
--- services/payment/src/main/java/com/kamis/ecommerce/payment/PaymentService.java
+++ services/payment/src/main/java/com/kamis/ecommerce/payment/PaymentService.java
@@ -14,8 +14,10 @@ public class PaymentService {
     private final NotificationProducer notificationProducer;
 
     public Integer createPayment(PaymentRequest request) {
-        var payment =  repository.save(mapper.toPayment(request));
-        notificationProducer.sendNotification(
+
+        var payment = this.repository.save(this.mapper.toPayment(request));
+
+        this.notificationProducer.sendNotification(
                 new PaymentNotificationRequest(
                         request.orderReference(),
                         request.amount(),
